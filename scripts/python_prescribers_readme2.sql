2b1bda14e20f6c76b643f134649e1e1766c9efae
janvimehendale <janvimehendale@gmail.com>
2026-01-17 19:34:15 -0600

    Q2

--- scripts/python_prescribers_readme2.sql
+++ scripts/python_prescribers_readme2.sql
@@ -4,9 +4,12 @@
 -- 3. drug name - drug
 -- 4. drug name - prescription join drug
 
+
+
 SELECT CONCAT(prescriber.nppes_provider_first_name, ' ', prescriber.nppes_provider_last_org_name)
 	AS Provider_Name,
-	COUNT(prescription.drug_name) AS precription_count
+	--drug.drug_name,
+	SUM(prescription.total_claim_count) AS precription_count
 FROM prescriber
 LEFT JOIN prescription 
 ON prescription.npi = prescriber.npi
@@ -15,5 +18,6 @@ ON drug.drug_name = prescription.drug_name
 WHERE drug.opioid_drug_flag = 'Y'
 AND nppes_provider_state = 'TN'
 GROUP BY Provider_Name
+	--drug.drug_name
 ORDER BY precription_count DESC
 LIMIT 10;
